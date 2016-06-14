c5863368346e5f721e96095ee45ac9b0f303dbf3
高帅 <gao629935@163.com>
2016-06-14 16:26:51 +0800

    no message

--- src/test/resources/mysql.sql
+++ src/test/resources/mysql.sql
@@ -1,5 +1,6 @@
 SET FOREIGN_KEY_CHECKS=0;
 
+--用户信息
   drop table if exists users;
   create table `users`(
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -14,6 +15,7 @@ SET FOREIGN_KEY_CHECKS=0;
   PRIMARY KEY (`id`)
   );
 
+--用户的角色信息
   drop table if exists roles;
   create table `roles`(
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -23,14 +25,15 @@ SET FOREIGN_KEY_CHECKS=0;
   PRIMARY KEY (`id`)
   );
 
+--角色权限
   drop table if exists role_authority;
   create table `role_authority`(
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL,
-  `child_id` INT NULL,
   PRIMARY KEY (`id`)
   );
 
+--企业管理员绑定的公司信息
   drop table if exists company;
   create table `company`(
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -43,6 +46,7 @@ SET FOREIGN_KEY_CHECKS=0;
   `contract_no` VARCHAR(255) NULL,
   `sign_date` DATETIME NULL,
   `contract_end_date` DATETIME NULL,
+  `background_url` VARCHAR(255) NULL,
   PRIMARY KEY (`id`)
   );
 
@@ -101,6 +105,7 @@ SET FOREIGN_KEY_CHECKS=0;
   PRIMARY KEY (`id`)
   );
 
+--房间内设备每天的报警数量信息
   drop table if exists room_daily;
   create table `room_daily`(
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -111,6 +116,7 @@ SET FOREIGN_KEY_CHECKS=0;
   PRIMARY KEY (`id`)
   );
 
+--监测的类型
   drop table if exists inspect_type;
   create table `inspect_type` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -118,13 +124,16 @@ SET FOREIGN_KEY_CHECKS=0;
   PRIMARY KEY (`id`)
   );
 
+--设备的类型
   drop table if exists device_type;
   create table `device_type`(
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL,
+  `logo_url` VARCHAR(255) NULL,
   PRIMARY KEY (`id`)
   );
 
+--设备的检测项判定
   drop table if exists device_inspect;
   create table `device_inspect`(
   `id` INT NOT NULL,
@@ -133,15 +142,38 @@ SET FOREIGN_KEY_CHECKS=0;
   PRIMARY KEY (`id`)
   );
 
+--监控终端
+  drop table if exists monitor_device;
+  create table `monitor_device`(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `number` VARCHAR(255) NULL,
+  );
+
+--设备信息
   drop table if exists device;
   create table `device`(
   `id` INT NOT NULL AUTO_INCREMENT,
+  `number` VARCHAR(255) NULL,
   `name` VARCHAR(255) NULL,
   `device_type_id` INT NULL,
+  `create_date` DATETIME NULL,
+  `creator` VARCHAR(255) NULL,
+  `purchase_date` DATETIME NULL,
+  `photo` VARCHAR(255) NULL,
+  `manager_user_id` INT NULL,
+  
 
   );
 
-
+  drop table if exists file;
+  create table `file`(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(255) NULL,
+  `description` VARCHAR(500) NULL,
+  `enable` INT NULL DEFAULT 0,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`id`)
+  );
 
 
 
