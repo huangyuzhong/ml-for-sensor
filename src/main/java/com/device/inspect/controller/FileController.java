e393d1c7c4231a4f060cae37d53bcb7fef107b53
高帅 <gao629935@163.com>
2016-08-25 18:42:05 +0800

    no message

--- src/main/java/com/device/inspect/controller/FileController.java
+++ src/main/java/com/device/inspect/controller/FileController.java
@@ -3,6 +3,7 @@ package com.device.inspect.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.device.inspect.common.model.charater.User;
+import com.device.inspect.common.model.device.Device;
 import com.device.inspect.common.model.device.DeviceType;
 import com.device.inspect.common.model.firm.Building;
 import com.device.inspect.common.model.firm.Room;
@@ -18,6 +19,11 @@ import com.device.inspect.common.repository.firm.BuildingRepository;
 import com.device.inspect.common.repository.firm.RoomRepository;
 import com.device.inspect.common.repository.firm.StoreyRepository;
 import com.device.inspect.common.restful.RestResponse;
+import com.device.inspect.common.restful.device.RestDevice;
+import com.device.inspect.common.restful.device.RestDeviceType;
+import com.device.inspect.common.restful.firm.RestBuilding;
+import com.device.inspect.common.restful.firm.RestFloor;
+import com.device.inspect.common.restful.firm.RestRoom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
@@ -139,13 +145,14 @@ public class FileController {
                             building.setBackground("/photo/company/" + fileName);
 //                        userRepository.save(user);
                         }
-                        restResponse = new RestResponse("添加成功！",null);
+
 
                     }
                 }catch (ClassCastException e){
                     e.printStackTrace();
                 }
                 buildingRepository.save(building);
+                restResponse = new RestResponse("操作成功！",new RestBuilding(building));
             } else {
                 restResponse = new RestResponse("权限不足！",1005,null);
             }
@@ -167,7 +174,10 @@ public class FileController {
         PrintWriter out = response.getWriter();
         if (null == user)
             restResponse = new RestResponse("手机号出错！", null);
-        if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")) {
+        else if(null==param.get("buildId")){
+            restResponse = new RestResponse("楼建筑信息出错！", null);
+        }
+        else if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")) {
 
             Storey floor = new Storey();
 
@@ -216,20 +226,93 @@ public class FileController {
 //                    userRepository.save(user);
 
                     }
-                    restResponse = new RestResponse("添加成功！",null);
-
                 }
             }catch (ClassCastException e){
                 e.printStackTrace();
             }
-
             storeyRepository.save(floor);
+            restResponse = new RestResponse("操作成功！",new RestFloor(floor));
         } else {
             restResponse = new RestResponse("权限不足！",1005,null);
         }
         out.print(JSON.toJSONString(restResponse));
         out.flush();
         out.close();
+    }
+
+    @RequestMapping(value = "/create/device/{name}")
+    public void createDevice(@PathVariable String name,@RequestParam Map<String,String> param,
+                             HttpServletRequest request,HttpServletResponse response)
+            throws ServletException, IOException,SerialException{
+        User user = userRepository.findByName(name);
+        RestResponse restResponse = null;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Device device = new Device();
+        if (null == user)
+            restResponse = new RestResponse("用户信息出错！", null);
+        else if(null==param.get("roomId"))
+            restResponse = new RestResponse("房间信息信息出错！", null);
+        else if (null==param.get("typeId"))
+            restResponse = new RestResponse("设备种类信息出错！", null);
+        else if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")){
+            Room room = roomRepository.findOne(Integer.valueOf(param.get("roomId")));
+            DeviceType deviceType = deviceTypeRepository.findOne(Integer.valueOf(param.get("typeId"));
+            device.setCreateDate(new Date());
+            device.setCode(param.get("code"));
+            device.setAlterNum(null == param.get("alterNum") ? 30 : Integer.valueOf(param.get("alterNum")));
+            device.setDeviceType(deviceType);
+            device.setManager(null == param.get("managerId") ? user : userRepository.findOne(Integer.valueOf(param.get("managerId"))));
+            device.setxPoint(null == param.get("xPoint") ? 0 : Float.valueOf(param.get("xPoint")));
+            device.setyPoint(null == param.get("yPoint") ? 0 : Float.valueOf(param.get("yPoint")));
+            device.setName(param.get("name"));
+
+            try {
+                MultipartHttpServletRequest multirequest = (MultipartHttpServletRequest) request;
+                MultiValueMap<String, MultipartFile> map = multirequest.getMultiFileMap();
+                Set<String> keys = map.keySet();
+                for (String key : keys) {
+                    JSONObject jobj = new JSONObject();
+                    String path = "";
+
+                    path = request.getSession().getServletContext().getRealPath("/") + "photo/device/";
+                    File add = new File(path);
+                    if (!add.exists() && !add.isDirectory()) {
+                        add.mkdir();
+                    }
+
+                    List<MultipartFile> files = map.get(key);
+                    if (null != files && files.size() > 0) {
+                        MultipartFile file = files.get(0);
+//                String name  = file.getOriginalFilename();
+                        String fileName = UUID.randomUUID().toString() + ".jpg";
+                        InputStream is = file.getInputStream();
+                        File f = new File(path + fileName);
+                        FileOutputStream fos = new FileOutputStream(f);
+                        int hasRead = 0;
+                        byte[] buf = new byte[1024];
+                        while ((hasRead = is.read(buf)) > 0) {
+                            fos.write(buf, 0, hasRead);
+                        }
+                        fos.close();
+                        is.close();
+
+                        device.setPhoto("/photo/device/" + fileName);
+//                    userRepository.save(user);
+
+                    }
+                }
+            }catch (ClassCastException e){
+                e.printStackTrace();
+            }
+
+            deviceRepository.save(device);
+            restResponse = new RestResponse("操作成功！",new RestDevice(device));
+        }else {
+            restResponse = new RestResponse("权限不足！",1005,null);
+        }
+
+
     }
 
     @RequestMapping(value = "/create/room/{name}")
@@ -242,12 +325,15 @@ public class FileController {
         PrintWriter out = response.getWriter();
         if (null == user)
             restResponse = new RestResponse("手机号出错！", null);
-        if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")) {
+        else if(null==param.get("floorId")){
+            restResponse = new RestResponse("楼层信息出错！", null);
+        }
+        else if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")) {
 
             Room room = new Room();
 
-            if (null!=param.get("type")&&null!=param.get("roomId")&&param.get("type").equals("1")){
-                room = roomRepository.findOne(Integer.valueOf(param.get("floorId")));
+            if (null!=param.get("type")&&null!=param.get("floorId")&&param.get("type").equals("1")){
+                room = roomRepository.findOne(Integer.valueOf(param.get("roomId")));
             }else {
                 room.setCreateDate(new Date());
                 room.setDeviceNum(0);
@@ -290,13 +376,14 @@ public class FileController {
 
                         room.setBackground("/photo/company/" + fileName);
                     }
-                    restResponse = new RestResponse("添加成功！",null);
+//                    restResponse = new RestResponse("添加成功！",null);
                 }
             }catch (ClassCastException e){
                 e.printStackTrace();
             }
 
             roomRepository.save(room);
+            restResponse = new RestResponse("操作成功！",new RestRoom(room));
         } else {
             restResponse = new RestResponse("权限不足！",1005,null);
         }
@@ -315,10 +402,15 @@ public class FileController {
         PrintWriter out = response.getWriter();
         if (null == user)
             restResponse = new RestResponse("手机号出错！", null);
-        if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")) {
 
-            DeviceType deviceType = new DeviceType();
-            deviceType.setName(null == param.get("name") ? null : param.get("name"));
+        DeviceType deviceType = new DeviceType();
+        if (user.getRole().getRoleAuthority().getName().equals("FIRM_MANAGER")) {
+            if (null!=param.get("type")&&null!=param.get("deviceTypeId")&&param.get("type").equals("1")){
+                deviceType = deviceTypeRepository.findOne(Integer.valueOf(param.get("deviceTypeId")));
+            }else {
+                deviceType.setName(null == param.get("name") ? null : param.get("name"));
+            }
+
             try {
                 MultipartHttpServletRequest multirequest = (MultipartHttpServletRequest) request;
                 MultiValueMap<String, MultipartFile> map = multirequest.getMultiFileMap();
@@ -358,7 +450,7 @@ public class FileController {
                 e.printStackTrace();
 //                deviceType.setLogo("/photo/company/" + fileName);
             }
-            restResponse = new RestResponse("添加成功！",null);
+            restResponse = new RestResponse("操作成功！",new RestDeviceType(deviceType));
         } else {
             restResponse = new RestResponse("权限不足！",1005,null);
         }
