cd3093355787a20ff6720f65a6b282cc1193b685
高帅 <gao629935@163.com>
2016-10-27 15:13:45 +0800

    no message

--- src/main/java/com/device/inspect/common/model/charater/User.java
+++ src/main/java/com/device/inspect/common/model/charater/User.java
@@ -42,9 +42,9 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public String getPassword() {
         return password;
