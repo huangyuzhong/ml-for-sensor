dfb763c78f1bf655d5289639527eb03e3b00d849
wkj <843156108@qq.com>
2016-10-31 18:40:40 +0800

    no message

--- src/main/java/com/device/inspect/controller/FileController.java
+++ src/main/java/com/device/inspect/controller/FileController.java
@@ -802,56 +802,49 @@ public class FileController {
     @RequestMapping("/logo/company")
     public void uploadLogo(Principal principal,HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException,SerialException {
-        User user=userRepository.findByName(principal.getName());
+        User user = judgeByPrincipal(principal);
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
         RestResponse restResponse=null;
-        if (user==null){
-            new RestResponse("用户信息不存在！",1005,null);
-        }else {
-            if (user.getRoles()!=null){
-                if (user.getRoles().get(0).getRoleAuthority().getName().equals("FIRM_MANAGER")){
-                    Company company=companyRepository.findOne(user.getCompany().getId());
-                    if (company==null){
-                        restResponse=new RestResponse("公司不存在",1005,null);
-                    }else {
-                        MultipartHttpServletRequest multipartHttpServletRequest=(MultipartHttpServletRequest ) request;
-                        MultiValueMap<String,MultipartFile> map=multipartHttpServletRequest.getMultiFileMap();
-                        Set<String> keys = map.keySet();
-                        List<String> result=new ArrayList<String>();
-                        for (String key:keys){
-                            JSONObject jsonObject=new JSONObject();
-                            String path="";
-                            path=request.getSession().getServletContext().getRealPath("/")+"logo/company"+company.getId()+"/";
-                            File add=new File(path);
-                            if (!add.exists()&&!add.isDirectory()){
-                                add.mkdir();
-                            }
-                            List<MultipartFile> files=map.get(key);
-                            if (null!=files&&files.size()>0){
-                                MultipartFile file=files.get(0);
-                                String fileName=file.getOriginalFilename();
-                                InputStream is=file.getInputStream();
-                                File f=new File(path+fileName);
-                                FileOutputStream fos=new FileOutputStream(f);
-                                int hasRead = 0;
-                                byte[] buf = new byte[1024];
-                                while ((hasRead=is.read(buf))>0){
-                                    fos.write(buf, 0, hasRead);
-                                }
-                                fos.close();
-                                is.close();
-                                company.setLogo("/logo/company/"+company.getId()+"/"+fileName);
-                                companyRepository.save(company);
-                            }
-
-                        }
-                        out.print(JSON.toJSONString(new RestResponse("图片上传成功！", 0, new RestCompany(company))));
+        if(UserRoleDifferent.userFirmManagerConfirm(user)){
+            Company company=user.getCompany();
+            if (company==null) {
+                restResponse=new RestResponse("公司不存在",1005,null);
+            }else {
+                MultipartHttpServletRequest multipartHttpServletRequest=(MultipartHttpServletRequest ) request;
+                MultiValueMap<String,MultipartFile> map=multipartHttpServletRequest.getMultiFileMap();
+                Set<String> keys = map.keySet();
+                List<String> result=new ArrayList<String>();
+                for (String key:keys){
+                    JSONObject jsonObject=new JSONObject();
+                    String path="";
+                    path=request.getSession().getServletContext().getRealPath("/")+"company/"+company.getId()+"/";
+                    File add=new File(path);
+                    if (!add.exists()&&!add.isDirectory()){
+                        add.mkdir();
                     }
-                }else {
-                    new RestResponse("用户权限不足，无法上传公司logo!",1005,null);
+                    List<MultipartFile> files=map.get(key);
+                    if (null!=files&&files.size()>0){
+                        MultipartFile file=files.get(0);
+                        String fileName=file.getOriginalFilename();
+                        InputStream is=file.getInputStream();
+                        File f=new File(path+fileName);
+                        FileOutputStream fos=new FileOutputStream(f);
+                        int hasRead = 0;
+                        byte[] buf = new byte[1024];
+                        while ((hasRead=is.read(buf))>0){
+                            fos.write(buf, 0, hasRead);
+                        }
+                        fos.close();
+                        is.close();
+                        company.setLogo("/company/"+company.getId()+"/"+fileName);
+                        companyRepository.save(company);
+                    }
                 }
+                out.print(JSON.toJSONString(new RestResponse("图片上传成功！", 0, new RestCompany(company))));
             }
+        }else {
+            new RestResponse("用户权限不足，无法上传公司logo!",1005,null);
         }
         out.flush();
         out.close();
