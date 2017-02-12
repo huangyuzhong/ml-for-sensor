7a6ebada1c9563abb33ba26d70856768247aade7
zyclincoln <zyclincoln@gmail.com>
2017-02-12 14:24:52 +0800

    add socket message api for power info

--- src/main/java/com/device/inspect/controller/SocketMessageApi.java
+++ src/main/java/com/device/inspect/controller/SocketMessageApi.java
@@ -249,7 +249,74 @@ public class SocketMessageApi {
                 System.out.println("checked record: " + check);
                 inspectData.setResult(String.valueOf(check));
                 deviceInspect.setCorrectionValue(check);
-            } else{
+            }
+            else if(monitorTypeCode.equals("08") || monitorTypeCode.equals("09")){
+                inspectData.setCreateDate(new Date());
+                inspectData.setDevice(device);
+                inspectData.setDeviceInspect(deviceInspect);
+                //添加测量原值
+                inspectData.setRealValue(String.valueOf(first));
+                record = Float.valueOf(first)*250*20/18000000;
+                deviceInspect.setOriginalValue(record);
+                check=record-(deviceInspect.getZero());
+                inspectData.setResult(String.valueOf(check));
+                deviceInspect.setCorrectionValue(check);
+            }
+            else if(monitorTypeCode.equals("0a")){
+                inspectData.setCreateDate(new Date());
+                inspectData.setDevice(device);
+                inspectData.setDeviceInspect(deviceInspect);
+                //添加测量原值
+                int value = ByteAndHex.byteArrayToInt(ByteAndHex.hexStringToBytes(fisrtData), 0, 2);
+                inspectData.setRealValue(String.valueOf(value));
+                record = Float.valueOf(value)*250;
+                deviceInspect.setOriginalValue(record);
+                check=record-(deviceInspect.getZero());
+                inspectData.setResult(String.valueOf(check));
+                deviceInspect.setCorrectionValue(check);
+
+                DeviceInspect deviceInspect2 = deviceInspectRepository.
+                        findByInspectTypeIdAndDeviceId(13, device.getId());
+                InspectData inspectData2 = new InspectData();
+                inspectData2.setCreateDate(new Date());
+                inspectData2.setDevice(device);
+                inspectData2.setDeviceInspect(deviceInspect2);
+                value = ByteAndHex.byteArrayToInt(ByteAndHex.hexStringToBytes(fisrtData), 2, 2);
+                record = Float.valueOf(value)*250;
+                check=record-(deviceInspect.getZero());
+                inspectData2.setRealValue(String.valueOf(value));
+                deviceInspect2.setOriginalValue(record);
+                inspectData2.setResult(String.valueOf(check));
+                deviceInspect2.setCorrectionValue(check);
+            }
+            else if(monitorTypeCode.equals("0b")){
+                inspectData.setCreateDate(new Date());
+                inspectData.setDevice(device);
+                inspectData.setDeviceInspect(deviceInspect);
+                //添加测量原值
+                int value = ByteAndHex.byteArrayToInt(ByteAndHex.hexStringToBytes(fisrtData), 0, 2);
+                inspectData.setRealValue(String.valueOf(value));
+                record = Float.valueOf(value)*250*20;
+                deviceInspect.setOriginalValue(record);
+                check=record-(deviceInspect.getZero());
+                inspectData.setResult(String.valueOf(check));
+                deviceInspect.setCorrectionValue(check);
+
+                DeviceInspect deviceInspect2 = deviceInspectRepository.
+                        findByInspectTypeIdAndDeviceId(15, device.getId());
+                InspectData inspectData2 = new InspectData();
+                inspectData2.setCreateDate(new Date());
+                inspectData2.setDevice(device);
+                inspectData2.setDeviceInspect(deviceInspect2);
+                value = ByteAndHex.byteArrayToInt(ByteAndHex.hexStringToBytes(fisrtData), 2, 2);
+                record = Float.valueOf(value)*250*20;
+                check=record-(deviceInspect.getZero());
+                inspectData2.setRealValue(String.valueOf(value));
+                deviceInspect2.setOriginalValue(record);
+                inspectData2.setResult(String.valueOf(check));
+                deviceInspect2.setCorrectionValue(check);
+            }
+            else{
                 inspectData.setCreateDate(new Date());
                 inspectData.setDevice(device);
                 inspectData.setDeviceInspect(deviceInspect);
