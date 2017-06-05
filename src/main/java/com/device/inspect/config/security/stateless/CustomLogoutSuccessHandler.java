a696b6bd0cf55e5bdac55afe42684bbf60cfa420
tobyfan1980 <tobyfan1980@gmail.com>
2017-06-05 07:06:10 +0000

    LAB-183, add login/logout record to influx db

---
+++ src/main/java/com/device/inspect/config/security/stateless/CustomLogoutSuccessHandler.java
@@ -0,0 +1,51 @@
+package com.device.inspect.config.security.stateless;
+
+import com.device.inspect.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import org.springframework.security.core.Authentication;
+
+import java.io.IOException;
+import java.security.Principal;
+
+/**
+ * Created by gxu on 6/5/17.
+ */
+public class CustomLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    protected static Logger logger = LogManager.getLogger(CustomLogoutSuccessHandler.class);
+    @Override
+    public void onLogoutSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication)
+            throws IOException, ServletException {
+
+        Principal principal = request.getUserPrincipal();
+
+        String userName = "Anonymous";
+
+
+
+        if(authentication != null){
+            userName =  authentication.getName();
+        }
+
+
+        if(Application.influxDBManager.writeAPIOperation(System.currentTimeMillis(), userName, request.getRequestURL().toString(), response.getStatus(), 0)){
+            logger.info(String.format("+++ successfully write to influxdb -- Executing %s [%s] takes %d ms, return code: %d", request.getRequestURL().toString(), userName, 0, response.getStatus()));
+        }
+        else{
+            logger.warn(String.format("+++ Failed to write influxdb -- Executing %s [%s] takes %d ms, return code: %d", request.getRequestURL().toString(), userName, 0, response.getStatus()));
+        }
+
+        super.onLogoutSuccess(request, response, authentication);
+    }
+}
