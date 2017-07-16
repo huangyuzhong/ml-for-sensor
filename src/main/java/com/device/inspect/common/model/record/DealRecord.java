992f833b48feb7cb14761edcc67f8a7ebc16b205
Ubuntu <ilabservice@ils-test-backend-vm1.tbs1sgrdmjjehd1kjyqwdzrmab.bx.internal.chinacloudapp.cn>
2017-07-16 03:52:37 +0000

    LAB-261 fix bug in model

--- src/main/java/com/device/inspect/common/model/record/DealRecord.java
+++ src/main/java/com/device/inspect/common/model/record/DealRecord.java
@@ -21,11 +21,11 @@ public class DealRecord {
     private Device device;
 
     @ManyToOne(targetEntity = User.class)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", insertable=false, updatable=false)
     private User lessor;
 
     @ManyToOne(targetEntity = User.class)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", insertable=false, updatable=false)
     private User lessee;
 
     @Column(name = "price")
