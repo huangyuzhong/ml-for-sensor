fb99c3030a31e85b009506a286b36ce72b89a058
hwd <1003186308@qq.com>
2017-09-20 15:38:59 +0800

    由于device字段太多，为了利于查询id,name,model字段，单独创建两个pojo类，接口返回值改为对象集合

---
+++ src/main/java/com/device/inspect/common/model/device/DeviceModel.java
@@ -0,0 +1,27 @@
+package com.device.inspect.common.model.device;
+
+
+/**
+ * Created by hwd on 2017/9/20.
+ */
+public class DeviceModel {
+
+    private Integer id;
+    private String model;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
